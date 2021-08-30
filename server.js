99f392f16a2a1341cb56e81e1b2c23ce9dc02f56
Lalitha Soumya Ayyagari <soumyaayyagari2310@gmail.com>
2021-08-30 22:03:48 +0530

    .

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/dist/first'));
+app.use(express.static(__dirname + '/InitiatingAngular/dist/first'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/dist/first/index.html');
+  res.sendFile(__dirname + '/InitiatingAngular/dist/first/index.html');
 })
 
 app.listen(port, () => {
